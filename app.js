d7e9d079bafc6ed7ae8c459ffb405d40c694c18e
Alex Seeley <alexseeley134@gmail.com>
2024-10-10 15:26:28 -0600

    Remove unnecessary comments

--- app.js
+++ app.js
@@ -9,11 +9,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`My first Express app - listening on port ${PORT}!`)
 );
-
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => res.send("Hello, world!"));
-
-// const PORT = 3000;
-// app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
